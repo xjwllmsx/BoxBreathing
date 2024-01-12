daf3af7c315df4b752e56dd76b915954b2fa2bc0
xjwllmsx <97003350+xjwllmsx@users.noreply.github.com>
2024-01-12 02:34:57 +0000

    Create countdown module
    The countdown function has been placed in its own module to make the main python code cleaner and easier to read.

---
+++ countdown.py
@@ -0,0 +1,22 @@
+# Import delay function from breathInAndOut
+from breathInAndOut import delay
+# Import clear() from replit module
+from replit import clear
+
+# Countdown function
+def countdown():
+  """Prints a countdown for the user"""
+  clear()
+  print("We'll begin in...")
+  delay()
+  print("5")
+  delay()
+  print("4")
+  delay()
+  print("3")
+  delay()
+  print("2")
+  delay()
+  print("1")
+  delay()
+  print("Let's begin!")
