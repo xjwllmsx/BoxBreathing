f38d1bffcbb505b6a0e09ecab23b161cf45fbf7f
Joseph <github@joseph-williams.me>
2025-04-27 23:05:48 -0500

    Refactor code to work with Juypter notebook for live demoing

--- clear.py
+++ clear.py
@@ -1,6 +1,7 @@
-from os import name, system
+# from os import name, system # Uncomment for use in terminal
+from IPython.display import clear_output  # Used for clearing Juypter Notebook cell
 
 
 def clear():
-    # checks for windows os, else runs clear function of mac and linux
-    _ = system("cls") if name == "nt" else system("clear")
+    # _ = system("cls") if name == "nt" else system("clear")  # checks for windows os, else runs clear function of mac and linux
+    clear_output(wait=True)  # clears Juypter notebook cell
--- main.py
+++ main.py
@@ -1,56 +1,60 @@
 # Import exit() from sys module
-from sys import exit
+# from sys import exit  # Uncomment to use with the terminal
 # Import clear()
 from clear import clear
+
 # Import all functions from howManyRounds module
 from howManyRounds import ask_for_rounds
+
 # Import round_of_breath from breathInAndOut module
 from breathInAndOut import round_of_breath
+
 # Import countdown from countdown module
 from countdown import countdown
 
 
-# Greet the user
-print("Welcome to the Box Breathing program!")
-# Ask for number of rounds
-num_of_rounds = ask_for_rounds()
-# Loop while the number of rounds is greater than 0
-while num_of_rounds > 0:
-  counter = 0
-  # Ask the user if they are ready to begin their breathing exercise
-  ready_to_start = input('Type "start" when you are ready to begin: ').lower()
-  # Start the breathing exercise if the user inputs 'start'
-  if ready_to_start == 'start':
-    # Display the countdown
-    countdown()
-    # Loop until number of rounds is equal to 0
-    while num_of_rounds != 0:
-      # Call round of breath function
-      round_of_breath()
-      # Subtract 1 from number of rounds
-      num_of_rounds -= 1
-      # Add 1 to counter
-      counter += 1
-    # Clear the console
-    clear()
-    # Print if the counter is greater than 1
-    if counter > 1:
-      print(f"Great work! You completed {counter} rounds of box breathing.")
-    # Print if the counter is not 1
-    else:
-      print(f"Great job! You completed {counter} round of box breathing.")
-    # Ask the user if they would like to continue with their breathing exercises
-    continue_breathing = input(
-        'Would you like to do additional rounds of breathing? Type "yes" or "no": '
-    ).lower()
-    # Clear the console and ask for number of rounds if the user wants to continue
-    if continue_breathing == 'yes':
-      clear()
-      num_of_rounds = ask_for_rounds()
-    # End the program if the user does not want to continue
-    elif continue_breathing == 'no':
-      print('Enjoy the rest of your day. Namaste!')
-      exit()
-    # Print if the user enters an invalid input
-    else:
-      print('Invalid input.')
+def box_breathing():
+    # Greet the user
+    print("Welcome to the Box Breathing program!")
+    # Ask for number of rounds
+    num_of_rounds = ask_for_rounds()
+    # Loop while the number of rounds is greater than 0
+    while num_of_rounds > 0:
+        counter = 0
+        # Ask the user if they are ready to begin their breathing exercise
+        ready_to_start = input('Type "start" when you are ready to begin: ').lower()
+        # Start the breathing exercise if the user inputs 'start'
+        if ready_to_start == "start":
+            # Display the countdown
+            countdown()
+            # Loop until number of rounds is equal to 0
+            while num_of_rounds != 0:
+                # Call round of breath function
+                round_of_breath()
+                # Subtract 1 from number of rounds
+                num_of_rounds -= 1
+                # Add 1 to counter
+                counter += 1
+            # Clear the console
+            clear()
+            # Print if the counter is greater than 1
+            if counter > 1:
+                print(f"Great work! You completed {counter} rounds of box breathing.")
+            # Print if the counter is not 1
+            else:
+                print(f"Great job! You completed {counter} round of box breathing.")
+            # Ask the user if they would like to continue with their breathing exercises
+            continue_breathing = input(
+                'Would you like to do additional rounds of breathing? Type "yes" or "no": '
+            ).lower()
+            # Clear the console and ask for number of rounds if the user wants to continue
+            if continue_breathing == "yes":
+                clear()
+                num_of_rounds = ask_for_rounds()
+            # End the program if the user does not want to continue
+            elif continue_breathing == "no":
+                print("Enjoy the rest of your day. Namaste!")
+                # exit()  # Uncomment for use in terminal
+            # Print if the user enters an invalid input
+            else:
+                print("Invalid input.")
